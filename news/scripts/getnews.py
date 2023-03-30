5f20da0ea3bceec14ab803c2db2a7805fc8aaf87
mel-fayne <melaniefayne33@gmail.com>
2023-03-30 14:58:00 +0300

    implement news tagging

--- news/scripts/getnews.py
+++ news/scripts/getnews.py
@@ -66,3 +66,46 @@ for source in sources:
             news_item["header_img"] = getImageUrl(entry.link)
 
         news.append(news_item)
+
+# ----------- Step Two: Tag News & Articles ------------------
+
+TECH_TAGS = {
+    'Entertainment': ['Netflix', 'Spotify'],
+    'Gaming': ['Gaming', 'PlayStation', 'PS5', 'TV', 'games', 'controllers', 'Unity'],
+    'AI & ML': ['Artificial', 'Intelligence', 'AI', 'openAI', 'chatGPT', 'FaceIO', 'Facial', 'Recognition'],
+    'Big Data': ['Data', 'science', 'Data', 'analytics', 'Jupyter', 'Julia', 'R'],
+    'Chatbots': ['ChatGPT', 'chatbot', 'openAI', 'bots', 'Bing'],
+    'BigTech': ['Amazon', 'Meta', 'Google', 'Apple', 'Facebook', 'Twitter', 'Meta', 'Tesla', 'Microsoft', 'Netflix', 'Spotify', 'IBM', 'Silicon', 'YouTube'],
+    'DevOps': ['API', 'Testing', 'Pipeline', 'Git', 'Debugging'],
+    'OS': ['Windows', 'iOS', 'OS', 'Ubuntu'],
+    'Mobile Dev': ['Kotlin', 'Flutter', 'Android'],
+    'Web Dev': ['React', 'Javascript', 'JS', 'HTML', 'CSS', 'Angular'],
+    'Programming': ['Python', 'Framework', 'Git', 'programming', 'JSON', 'Scrum', 'Agile', 'Java', 'code', 'debug'],
+    'Energy': ['Renewable', 'Energy', 'Coal', 'Power'],
+    'Electric Vehicles': ['Lamborghini', 'Plug-in', 'Hybrid', 'Supercar', 'Kia', 'EV9', 'electric', 'SUV', 'Level', '3', 'autonomy', 'range', 'Lucid', 'Motors'],
+    'Events': ['WWDC', 'Event', 'Summit'],
+    'Gadgets': ['M2', 'Pro', 'Mac', 'mini', 'Sony', 'ZV-E1', 'vlogging', 'Logitech', 'Zone', 'Learn', 'headset', 'HP', 'laser', 'printer', 'TV'', ', '5G', 'smartphone'],
+    'Graphics': ['Render', 'graphics'],
+    'Cybersecurity': ['cyber', 'security', 'hack', 'phishing', 'breaches'],
+    'Business Tech': ['venture', 'capital', 'Silicon', 'Valley', 'Bank', 'Funding', 'Seed', 'Business', 'fintech', 'Startup', 'Ecosystem'],
+    'Blockchain': ['Cryptocurrency', 'Crypto', 'Bitcoin', 'Web3'],
+    'Database': ['Database', 'PostgreSQL'],
+    'Networking': ['Hub', 'Switch', 'Router', 'Modem'],
+    'Career': ['Career', 'job', 'Interview', 'CV'],
+    'Cloud': ['storage', 'cloud', 'crowdsource', 'AWS', 'Amazon', 'Web', 'Services'],
+}
+
+def get_tech_topics(title):
+    keywords = title.lower().split()
+    
+    tech_matches = []
+    for topic, topic_keywords in TECH_TAGS.items():
+        for keyword in keywords:
+            if keyword in topic_keywords:
+                tech_matches.append(topic)
+    
+    return tech_matches
+
+# tag news items
+for item in news:
+    item['tags'] = get_tech_topics(item['title'])
