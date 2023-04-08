10fc43faf11b4c187f3c2799e8698bf098ceac8b
mel-fayne <melaniefayne33@gmail.com>
2023-04-08 15:59:50 +0300

    implement tech job views

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from news.views import AllNewsPiecesView, NewsPiecesByTagView, AllTechEventsView, TechEventsByDateView 
+from news.views import AllNewsPiecesView, NewsPiecesByTagView, AllTechEventsView, TechEventsByDateView, AllTechJobsView, TechJobsByAreaView
 
 urlpatterns = [
     path('all_news', AllNewsPiecesView.as_view()),
@@ -8,4 +8,7 @@ urlpatterns = [
 
     path('all_events', AllTechEventsView.as_view()),
     path('filter_events', TechEventsByDateView.as_view()),
+
+    path('all_jobs', AllTechJobsView.as_view()),
+    path('filter_jobs', TechJobsByAreaView.as_view()),
 ]
--- news/views.py
+++ news/views.py
@@ -2,13 +2,15 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from datetime import datetime, timedelta
-from news.serializers import NewsPieceSerializer, TechEventSerializer
-from news.models import NewsPiece, TechEvent
+from news.serializers import NewsPieceSerializer, TechEventSerializer, TechJobSerializer
+from news.models import NewsPiece, TechEvent, TechJob
 
 # ---------------------- NewsPiece Views ------------------------------------
+
+
 class NewsPiecesByTagView(APIView):
     def get(self, request):
-        tags = request.GET.get('tag')
+        tags = request.GET.get('tags')
 
         if tags:
             tag_list = tags.split(',')
@@ -18,6 +20,7 @@ class NewsPiecesByTagView(APIView):
         else:
             return Response('no news found')
 
+
 class AllNewsPiecesView(APIView):
     def get(self):
         news = NewsPiece.objects.all().order_by('date_created')
@@ -25,16 +28,41 @@ class AllNewsPiecesView(APIView):
         return Response(news_serializer.data)
 
 # ---------------------- TechEvent Views ------------------------------------
+
+
 class TechEventsByDateView(APIView):
     def get(self, request):
         period = int(request.GET.get('period'))
-        lastDate =  datetime.date.today() - timedelta(days=period)
+        lastDate = datetime.date.today() - timedelta(days=period)
         events = TechEvent.objects.filter(date__mt=lastDate)
         events_serializer = TechEventSerializer(events, many=True)
         return Response(events_serializer.data)
-        
+
+
 class AllTechEventsView(APIView):
     def get(self):
         events = TechEvent.objects.all().order_by('date')
         events_serializer = TechEventSerializer(events, many=True)
         return Response(events_serializer.data)
+
+# ---------------------- TechJobs Views ------------------------------------
+
+
+class TechJobsByAreaView(APIView):
+    def get(self, request):
+        areas = request.GET.get('areas')
+
+        if jobs_serializer:
+            area_list = areas.split(',')
+            jobs = NewsPiece.objects.filter(areas__in=area_list)
+            jobs_serializer = TechJobSerializer(jobs, many=True)
+            return Response(jobs_serializer.data)
+        else:
+            return Response('no jobs found')
+
+
+class AllTechJobsView(APIView):
+    def get(self):
+        jobs = TechJob.objects.all().order_by('posted')
+        jobs_serializer = TechJobSerializer(jobs, many=True)
+        return Response(jobs_serializer.data)
