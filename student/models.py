d054ff5da7d5f6f8f6d1081de487be2c042890bf
mel-fayne <melaniefayne33@gmail.com>
2023-04-10 12:10:00 +0300

    add account verification fields to student model

--- student/models.py
+++ student/models.py
@@ -3,15 +3,17 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
   
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50, null=True)
+    last_name = models.CharField(max_length=50, null=True)
     email = models.CharField(max_length=200, unique=True)
 
     # ------------- auth fields --------------
     user_token = models.CharField(max_length=200, null=True)
     last_active = models.CharField(max_length=200, null = True)
+    isVerified = models.BooleanField(default=False)
+    verify_otp = models.IntegerField(null=True)
     reset_otp = models.IntegerField(null=True)
-    reset_time = models.DateTimeField(null=True)
+    reset_expiry = models.DateTimeField(null=True)
 
     # ------------- news setting fields -------------------
     news_history = models.JSONField(default=list, null=True)
@@ -22,9 +24,15 @@ class Student(models.Model):
     def getId(self):
         return self.id
     
+    def getIsVerified(self):
+        return self.isVerified
+    
+    def getVerifyCode(self):
+        return self.verify_otp
+    
     def getOTP(self):
         return self.reset_otp
     
     def getOTPTime(self):
-        return self.reset_time
+        return self.reset_expiry
     
