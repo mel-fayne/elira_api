d3b6ca39d6583ab80a387615e3996bc4e558fb54
mel-fayne <melaniefayne33@gmail.com>
2023-04-12 12:41:12 +0300

    setup technical profile views

--- student/views/views.py
+++ student/views/views.py
@@ -1,8 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from student.serializers import StudentSerializer
-from student.models import Student
+from student.serializers import StudentSerializer, TechnicalProfileSerializer
+from student.models import Student, TechnicalProfile
 
 # Create your views here.
 
@@ -18,7 +18,35 @@ class StudentView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+
+class TechnicalProfileView(APIView):
+    def get(self, *args, **kwargs):
+        tech_profile = TechnicalProfile.objects.filter(id=self.kwargs['student_id']).first()
+
+        # TODO: check for consistency in fields
+        profile_data = {}
+        
+        serializer = TechnicalProfileSerializer(tech_profile, data=profile_data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        tech_profile = TechnicalProfile.objects.filter(id=self.kwargs['student_id']).first()
+        serializer = TechnicalProfileSerializer(tech_profile)
+        return Response(serializer.data)
     
+    def post(self, request):
+
+        # TODO: populate fields
+        # TODO: calculate technical score
+        # TODO: get specialisation rank
+
+        serializer = TechnicalProfileSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+
 class AllStudentsView(APIView):
     def get(self, request):
         students = Student.objects.all()
