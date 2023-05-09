cf649999464167a0146a648d7b93c342653f6ae2
mel-fayne <melaniefayne33@gmail.com>
2023-05-09 13:16:24 +0300

    correct security check fields in student model

--- student/models/studentModels.py
+++ student/models/studentModels.py
@@ -27,13 +27,13 @@ class Student(models.Model):
     # ------------- auth fields --------------
     user_token = models.CharField(max_length=200, null=True)
     last_active = models.CharField(max_length=200, null=True)
-
-    childhod_nicknamefirst_pet = models.CharField(max_length=200, null=True)
-    childhood_street = models.CharField(max_length=200, null=True)
-    first_teacher = models.CharField(max_length=200, null=True)
-    favourite_flavour = models.CharField(max_length=200, null=True)
-    childhod_nickname = models.CharField(max_length=200, null=True)
-    first_phone = models.CharField(max_length=200, null=True)
+    
+    first_pet = models.CharField(max_length=50, null=True)
+    childhood_street = models.CharField(max_length=50, null=True)
+    first_teacher = models.CharField(max_length=50, null=True)
+    favourite_flavour = models.CharField(max_length=50, null=True)
+    childhod_nickname = models.CharField(max_length=50, null=True)
+    first_phone = models.CharField(max_length=50, null=True)
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
